5c2c6e6031e81f3189c8974d48a310db6d406241
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-09-28 15:41:27 +0200

    Push class

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,11 @@
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Main {
+    public static void main(String[] args) throws RemoteException {
+        Registry reg= LocateRegistry.createRegistry(2001);
+        ObjetDistant d = new ObjetDistant(10001);
+        reg.rebind("MonOD",d);
+    }
 }
