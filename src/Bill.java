3b654b062e293e93c7c5f24376ff3ff6379ba1f5
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-09-28 15:29:21 +0200

    add class

---
+++ src/Bill.java
@@ -0,0 +1,12 @@
+import java.math.BigInteger;
+
+public class Bill {
+    String movieName;
+    BigInteger outrageousPrice;
+
+
+    public Bill(String movieName, BigInteger outrageousPrice) {
+        this.movieName = movieName;
+        this.outrageousPrice = outrageousPrice;
+    }
+}
---
+++ src/VODService.java
@@ -0,0 +1,7 @@
+public class VODService {
+    List<MovieDesc> viewCatalog(){return null;};
+    Bill playmovie(String isbn, IClientBox box){return null;};
+
+    public VODService() {
+    }
+}
