18758f1fe77cb6fe3a03be2bc72ee0e3f0a47c15
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-09-28 15:33:26 +0200

    Push class

--- src/Connection.java
+++ src/Connection.java
@@ -1,11 +1,12 @@
+import exception.InvalidCredentialsException;
+import exception.SignInFailed;
+
 import java.lang.reflect.Array;
 
 public class Connection {
     Array clientList;
-    //throws SignInFailed
-    boolean signIn (String mail, String pwd){return true;}
+    boolean signIn (String mail, String pwd) throws SignInFailed {return true;}
     //(eg, if a client with this email already exists)
-    //throws InvalidCredentialsException
-    IVODService login(String mail, String pwd){return null;}
+    IVODService login(String mail, String pwd) throws InvalidCredentialsException {return null;}
     //(if mail/pwd don't match)
 }
