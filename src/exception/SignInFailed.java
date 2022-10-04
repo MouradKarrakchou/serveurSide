7867d3aaa5e334f069c175194c39d35b78418cdb
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-10-04 13:08:06 +0200

    git fatigué

--- src/exception/SignInFailed.java
+++ src/exception/SignInFailed.java
@@ -6,6 +6,6 @@ import java.rmi.RemoteException;
  */
 public class SignInFailed extends Exception {
     public SignInFailed() {
-        super("Erreur de lors de l'inscription, un compte avec le même email existe déjà");
+        super("Erreur lors de l'inscription, un compte avec le même email existe déjà");
     }
 }
