c1c207d9caf0f4e94fde5aa5b29329a063407fe8
LeBihanLeo <leo.le-bihan@etu.univ-cotedazur.fr>
2022-10-04 13:25:45 +0200

    git fatigué

--- src/IConnection.java
+++ src/IConnection.java
@@ -4,6 +4,21 @@ import exception.SignInFailed;
 import java.rmi.RemoteException;
 
 public interface IConnection extends java.rmi.Remote {
+    /**
+     * Register a client
+     * @param mail
+     * @param pwd
+     * @return true if the client is correctly register, otherwise false
+     * @throws SignInFailed, when the client try to register with a mail already know
+     */
     boolean signIn(String mail,String pwd) throws RemoteException, SignInFailed;
+
+    /**
+     * Login the client into the Vod service
+     * @param mail
+     * @param pwd
+     * @return IVODService, that the client will use to interact with the systeme
+     * @throws InvalidCredentialsException, when the client try to login with wrong mail and password
+     */
     IVODService login(String mail,String pwd) throws RemoteException, InvalidCredentialsException;
 }
