fa8338ac65ac5d122fcd42ea95ec6a69cbec3ceb
MouradKarrakchou <mourad.karrakchou@gmail.com>
2022-10-04 13:25:01 +0200

    Push class

--- src/IVODService.java
+++ src/IVODService.java
@@ -5,5 +5,12 @@ import java.util.List;
 
 public interface IVODService extends java.rmi.Remote {
     List<MovieDesc> viewCatalog() throws RemoteException;
+    /**
+     * Find the movie by it isbn and create a thread that stream the movie
+     * @param isbn
+     * @param box
+     * @return
+     * @throws InvalidISBN
+     */
     Bill playMovie(String isbn, IClientBox box) throws RemoteException, InvalidISBN;
 }
